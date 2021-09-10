11af56fa952b98d9f954ba8486a5d24efb1a1deb
yangfan36 <yangfan36@xiaomi.com>
2021-09-10 11:14:17 +0800

    init

--- App.js
+++ App.js
@@ -1,23 +1,47 @@
 // In App.js in a new project
 
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 // 指向当前的组件、类似于reactRoute
-function HomeScreen() {
+function HomeScreen(state) {
+  const { navigation } = state
+
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Home Screen</Text>
+      <Button 
+        title='点击我跳转详情'
+        onPress={() => navigation.navigate('Details')}    
+      />
     </View>
   );
 }
 
-function DetailsScreen(){
+/**
+ * navigate: 如果跳转屏幕的时候 你已经在当前屏幕、那么就什么也不要做；
+ * push: 类似于react route的push、这个很好理解
+ * popToTop: 返回到堆栈中的第一个屏幕。
+ */
+
+function DetailsScreen( state ){
+  const { navigation } = state
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Details Screen</Text>
+      <Button
+        title="Go to navigate Details" 
+        onPress={() => navigation.navigate('Details')}
+      />
+      <Button
+        title="Go to push Details" 
+        onPress={() => navigation.push('Details')}
+      />
+      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+      <Button title="Go popToTop" onPress={() => navigation.popToTop()} />
     </View>
   );
 }
