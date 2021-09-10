c6c32db513f284178fa474c090569b2a025b3abd
yangfan36 <yangfan36@xiaomi.com>
2021-09-10 17:02:45 +0800

    init

--- App.js
+++ App.js
@@ -1,64 +1,77 @@
 import * as React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, Image } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-/**
- * 1、参数作为函数对象的第二个参数传递过去、navigation.navigate('RouteName', {  params go here  })
- * 2、setParams 更新参数对象
- * 3、initialParams 初始化参数
- */
 
-function HomeScreen(state) {
-  const { navigation,route } = state
+function LogoTitle(props) { 
+  return(
+    <>
+      <Text style={{ color:'#fff'}}>{props.children}</Text>
+      <Image
+        style={{ width: 50, height: 50 }}
+        source={{
+          uri: 'https://reactnative.dev/img/tiny_logo.png',
+        }}
+        />
+    </>
+  )
+}
 
+function HomeScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>默认参数：{JSON.stringify(route.params.id)}</Text>
-      <Text>详情传递的参数：{JSON.stringify(route)}</Text>
-      <Button 
-        title='点击我跳转详情'
-        onPress={() => 
-          navigation.navigate('Details')
-        }    
-      />
-      <Button 
-        title='更新参数'
-        onPress={() => 
-          navigation.setParams({
-            otherParam:"我更新一下"
-          })
-        }    
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Profile"
+        onPress={() =>
+          navigation.navigate('Profile', { name: '自定义头部' })
+        }
       />
     </View>
   );
 }
 
-function DetailsScreen( state ){
-  const { navigation, route } = state
+function ProfileScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>首页传递的参数：{JSON.stringify(route)}</Text>
-      <Button 
-        title='点击我跳转首页'
-        onPress={() => 
-            navigation.navigate('Home', {
-            otherParam: '我是首页',
-          })
-        }    
+      <Text>Profile screen</Text>
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+      <Button
+        title="点击我更新头部"
+        onPress={() => navigation.setOptions({ title: '更新头部' })}
       />
     </View>
   );
 }
-// 创建一个路由的对象
+
 const Stack = createNativeStackNavigator();
 
 function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" initialParams={{ id: 999}}  options={{ title:"我是首页"}} component={HomeScreen} />
-        <Stack.Screen name="Details" options={{ title:"我是详情"}}  component={DetailsScreen} />
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ 
+            headerTitle: props => <LogoTitle {...props} /> ,
+            title: '首页',
+            headerStyle:{
+              backgroundColor:'red',
+              
+            },
+            headerTintColor:'#fff',
+            headerTitleStyle:{
+              fontWeight:"blod"
+            }
+          }}
+        />
+        <Stack.Screen
+          name="Profile"
+          component={ProfileScreen}
+          options={({ route }) => ({ title: route.params.name })}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- readme.md
+++ readme.md
@@ -21,3 +21,4 @@ expo start
 - `main-1` 基本的路由展示配置
 - `main-2` 路由之间的跳转、push、goback、navigate、popToTop
 - `main-3` 参数传递路由 params、setParams、initialParams 
+- `main-4` 配置标题栏 
