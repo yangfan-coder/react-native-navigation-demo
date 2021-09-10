cb3ea1705fab33154b62fa9520e320457a66b2d0
yangfan36 <yangfan36@xiaomi.com>
2021-09-10 11:44:48 +0800

    init

--- App.js
+++ App.js
@@ -1,47 +1,52 @@
-// In App.js in a new project
-
 import * as React from 'react';
 import { View, Text, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-// 指向当前的组件、类似于reactRoute
+/**
+ * 1、参数作为函数对象的第二个参数传递过去、navigation.navigate('RouteName', {  params go here  })
+ * 2、setParams 更新参数对象
+ * 3、initialParams 初始化参数
+ */
+
 function HomeScreen(state) {
-  const { navigation } = state
+  const { navigation,route } = state
 
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
+      <Text>默认参数：{JSON.stringify(route.params.id)}</Text>
+      <Text>详情传递的参数：{JSON.stringify(route)}</Text>
       <Button 
         title='点击我跳转详情'
-        onPress={() => navigation.navigate('Details')}    
+        onPress={() => 
+          navigation.navigate('Details')
+        }    
+      />
+      <Button 
+        title='更新参数'
+        onPress={() => 
+          navigation.setParams({
+            otherParam:"我更新一下"
+          })
+        }    
       />
     </View>
   );
 }
 
-/**
- * navigate: 如果跳转屏幕的时候 你已经在当前屏幕、那么就什么也不要做；
- * push: 类似于react route的push、这个很好理解
- * popToTop: 返回到堆栈中的第一个屏幕。
- */
-
 function DetailsScreen( state ){
-  const { navigation } = state
+  const { navigation, route } = state
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Details Screen</Text>
-      <Button
-        title="Go to navigate Details" 
-        onPress={() => navigation.navigate('Details')}
+      <Text>首页传递的参数：{JSON.stringify(route)}</Text>
+      <Button 
+        title='点击我跳转首页'
+        onPress={() => 
+            navigation.navigate('Home', {
+            otherParam: '我是首页',
+          })
+        }    
       />
-      <Button
-        title="Go to push Details" 
-        onPress={() => navigation.push('Details')}
-      />
-      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
-      <Button title="Go back" onPress={() => navigation.goBack()} />
-      <Button title="Go popToTop" onPress={() => navigation.popToTop()} />
     </View>
   );
 }
@@ -52,7 +57,7 @@ function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" options={{ title:"我是首页"}} component={HomeScreen} />
+        <Stack.Screen name="Home" initialParams={{ id: 999}}  options={{ title:"我是首页"}} component={HomeScreen} />
         <Stack.Screen name="Details" options={{ title:"我是详情"}}  component={DetailsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
--- readme.md
+++ readme.md
@@ -20,3 +20,4 @@ expo start
 - `main` 初始项目分支
 - `main-1` 基本的路由展示配置
 - `main-2` 路由之间的跳转、push、goback、navigate、popToTop
+- `main-3` 参数传递路由 params、setParams、initialParams 
