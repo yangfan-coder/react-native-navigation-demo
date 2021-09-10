7fbcd41cc203d6da8eb58ae93c5f8705479b60e1
yangfan36 <yangfan36@xiaomi.com>
2021-09-10 10:58:04 +0800

    init

--- App.js
+++ App.js
@@ -1,21 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// In App.js in a new project
 
-export default function App() {
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+// 指向当前的组件、类似于reactRoute
+function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function DetailsScreen(){
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+    </View>
+  );
+}
+// 创建一个路由的对象
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" options={{ title:"我是首页"}} component={HomeScreen} />
+        <Stack.Screen name="Details" options={{ title:"我是详情"}}  component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
---
+++ readme.md
@@ -0,0 +1,22 @@
+# rn navigation练习
+
+<br />
+
+## 执行命令
+
+参考 [expo-cli](https://docs.expo.dev/get-started/installation/)
+```shell
+npm install --global expo-cli
+expo init my-app
+cd my-app
+expo start
+```
+关于[cli的常用命令](https://docs.expo.dev/workflow/expo-cli/)
+
+<br />
+
+## 分支
+
+- `main` 初始项目分支
+- `main-1` 基本的路由展示配置
+
